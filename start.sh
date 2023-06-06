046a735cff2e8a8628b08d97da77f0eb5c70e75f
Arunbotz <135454216+Arunbotz@users.noreply.github.com>
2023-06-06 19:08:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,23 @@
 if [ -z $UPSTREAM_REPO ]
+
 then
+
   echo "Cloning main Repository"
-  git clone https://github.com/Arunbotz/LazyPrincessV2/LazyPrincessV2.git /LazyPrincessV2
+
+  git clone https://github.com/Arunbotz/LazyPrincessV2/SKY-FILTER-HD.git /SKY-FILTER-HD
+
 else
+
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincessV2
+
+  git clone $UPSTREAM_REPO /SKY-FILTER-HD
+
 fi
-cd /LazyPrincessV2
+
+cd /SKY-FILTER-HD
+
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+
+echo "🍃Starting Bot🍃...."
+
 python3 bot.py
